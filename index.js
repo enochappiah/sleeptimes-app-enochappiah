6e72c84143287a2e899647f2d4bd8fa4937557ea
eappiah7 <eappiah77@yahoo.com>
2023-09-02 16:32:15 -0400

    Bug fix for 12:15
    Edge case with 12AM, needs to be interpreted as 0
    
    [Ticket: X]

--- index.js
+++ index.js
@@ -26,11 +26,19 @@ function calcBedTimes() {
   //extract desired wake up time from dropdown form  
   const wakeUpHour = document.getElementById("hour-dropdown").value;
   const wakeUpMinutes = document.getElementById("minute-dropdown").value;
-  const wakeUpDay = document.getElementById("ampm-dropdown").value;
+  //const wakeUpDay = document.getElementById("ampm-dropdown").value;
+//   if (wakeUpHour === "12") {
+//     wakeUpHour = 0;
+//   }
+
   
   //create new Date object for wakeuptime with accounted fall asleep time
   const wakeUpTime = new Date();
-  wakeUpTime.setHours(wakeUpHour);
+  if (wakeUpHour === "12") {
+    wakeUpTime.setHours(0);
+  } else {
+    wakeUpTime.setHours(wakeUpHour);
+  }
   wakeUpTime.setMinutes(wakeUpMinutes - 14);
   
 
